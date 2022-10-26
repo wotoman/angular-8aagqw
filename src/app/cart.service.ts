c3a1197e09b38d8d0c67098c2fea2a8c81397eca
wotoman <63972351+wotoman@users.noreply.github.com>
2022-10-26 13:28:02 +0200

    4 files modified, 4 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 import { Injectable } from '@angular/core';
 
@@ -5,7 +6,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class CartService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
   items: Product[] = [];
 
   addToCart(product: Product) {
@@ -19,5 +20,11 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    );
   }
 }
